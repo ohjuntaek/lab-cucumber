cc4058793bb52106946430c43601d19828e13b5e
ohjuntaekmb <hangsang090@marketboro.com>
2022-01-31 15:23:52 +0900

    first commit

---
+++ src/main/java/lab/junatek/labcucumber/LabCucumberApplication.java
@@ -0,0 +1,13 @@
+package lab.junatek.labcucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCucumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/junatek/labcucumber/LabCucumberApplicationTests.java
@@ -0,0 +1,14 @@
+package lab.junatek.labcucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabCucumberApplicationTests {
+
+	// test
+	@Test
+	void contextLoads() {
+	}
+
+}
